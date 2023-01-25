16f4d836f01c0c46be744bbd97e4d24b90e0b976
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-01-25 19:03:04 +0100

    done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,8 @@ export default class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -14,7 +15,7 @@ export default class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
